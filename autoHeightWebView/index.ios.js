c4d90c5de162dd624c867689f004b66d3ed3fa42
Harutyun Drnoyan <harutyun.drnoyan@gmail.com>
2019-02-10 01:45:28 +0100

    iOS specific workaround - https://github.com/facebook/react-native/issues/10865#issuecomment-269847703

--- autoHeightWebView/index.ios.js
+++ autoHeightWebView/index.ios.js
@@ -201,7 +201,19 @@ export default class AutoHeightWebView extends PureComponent {
           style={styles.webView}
           scrollEnabled={!!scrollEnabled}
           scalesPageToFit={scalesPageToFit}
-          injectedJavaScript={script}
+          injectedJavaScript={`(function() {
+            var originalPostMessage = window.postMessage;
+          
+            var patchedPostMessage = function(message, targetOrigin, transfer) { 
+              originalPostMessage(message, targetOrigin, transfer);
+            };
+          
+            patchedPostMessage.toString = function() { 
+              return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage'); 
+            };
+          
+            window.postMessage = patchedPostMessage;
+          })();${script}`}
           source={source}
           onNavigationStateChange={this.handleNavigationStateChange}
         />
